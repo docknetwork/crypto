7e8e16c2c1ab76bf2bf927d40a07292e59cb634e
lovesh <lovesh.bond@gmail.com>
2021-10-01 16:24:08 +0530

    Add feature to util repo
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- dock_crypto_utils/src/lib.rs
+++ dock_crypto_utils/src/lib.rs
@@ -1,5 +1,5 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod hashing_utils;
-// TODO: Feature gate this
+#[cfg(feature = "serde-support")]
 pub mod serde_utils;
--- dock_crypto_utils/src/serde_utils.rs
+++ dock_crypto_utils/src/serde_utils.rs
@@ -115,3 +115,121 @@ where
 {
     serializer.serialize_str(&error.to_string())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ark_bls12_381::Bls12_381;
+    use ark_ec::{PairingEngine, ProjectiveCurve};
+    use ark_std::collections::BTreeMap;
+    use ark_std::{
+        io::{Read, Write},
+        rand::{rngs::StdRng, SeedableRng},
+        UniformRand,
+    };
+    use serde::Deserialize;
+    use serde_with::serde_as;
+
+    type Fr = <Bls12_381 as PairingEngine>::Fr;
+    type G1Proj = <Bls12_381 as PairingEngine>::G1Projective;
+    type G2Proj = <Bls12_381 as PairingEngine>::G2Projective;
+
+    #[test]
+    fn serde_field() {
+        let mut rng = StdRng::seed_from_u64(0u64);
+
+        #[serde_as]
+        #[derive(
+            Clone,
+            Debug,
+            PartialEq,
+            Eq,
+            CanonicalSerialize,
+            CanonicalDeserialize,
+            Serialize,
+            Deserialize,
+        )]
+        pub struct Temp<F: Field> {
+            #[serde_as(as = "FieldBytes")]
+            single: F,
+            #[serde_as(as = "Vec<FieldBytes>")]
+            vec: Vec<F>,
+            #[serde_as(as = "BTreeMap<_, FieldBytes>")]
+            map: BTreeMap<usize, F>,
+        }
+
+        let mut map = BTreeMap::new();
+        map.insert(1, Fr::rand(&mut rng));
+        map.insert(3, Fr::rand(&mut rng));
+        map.insert(4, Fr::rand(&mut rng));
+        let t = Temp {
+            single: Fr::rand(&mut rng),
+            vec: vec![Fr::rand(&mut rng), Fr::rand(&mut rng), Fr::rand(&mut rng)],
+            map,
+        };
+        let t_ser = serde_json::to_string(&t).unwrap();
+        let t_deser = serde_json::from_str::<Temp<Fr>>(&t_ser).unwrap();
+        assert_eq!(t, t_deser);
+    }
+
+    #[test]
+    fn serde_affine_group() {
+        let mut rng = StdRng::seed_from_u64(0u64);
+
+        #[serde_as]
+        #[derive(
+            Clone,
+            Debug,
+            PartialEq,
+            Eq,
+            CanonicalSerialize,
+            CanonicalDeserialize,
+            Serialize,
+            Deserialize,
+        )]
+        pub struct Temp<G: AffineCurve> {
+            #[serde_as(as = "AffineGroupBytes")]
+            single: G,
+            #[serde_as(as = "Vec<AffineGroupBytes>")]
+            vec: Vec<G>,
+            #[serde_as(as = "BTreeMap<_, AffineGroupBytes>")]
+            map: BTreeMap<usize, G>,
+        }
+
+        let mut map = BTreeMap::new();
+        map.insert(1, G1Proj::rand(&mut rng).into_affine());
+        map.insert(3, G1Proj::rand(&mut rng).into_affine());
+        map.insert(4, G1Proj::rand(&mut rng).into_affine());
+        let t1 = Temp {
+            single: G1Proj::rand(&mut rng).into_affine(),
+            vec: vec![
+                G1Proj::rand(&mut rng).into_affine(),
+                G1Proj::rand(&mut rng).into_affine(),
+                G1Proj::rand(&mut rng).into_affine(),
+            ],
+            map,
+        };
+        let t_ser = serde_json::to_string(&t1).unwrap();
+        let t_deser =
+            serde_json::from_str::<Temp<<Bls12_381 as PairingEngine>::G1Affine>>(&t_ser).unwrap();
+        assert_eq!(t1, t_deser);
+
+        let mut map = BTreeMap::new();
+        map.insert(1, G2Proj::rand(&mut rng).into_affine());
+        map.insert(3, G2Proj::rand(&mut rng).into_affine());
+        map.insert(4, G2Proj::rand(&mut rng).into_affine());
+        let t2 = Temp {
+            single: G2Proj::rand(&mut rng).into_affine(),
+            vec: vec![
+                G2Proj::rand(&mut rng).into_affine(),
+                G2Proj::rand(&mut rng).into_affine(),
+                G2Proj::rand(&mut rng).into_affine(),
+            ],
+            map,
+        };
+        let t_ser = serde_json::to_string(&t2).unwrap();
+        let t_deser =
+            serde_json::from_str::<Temp<<Bls12_381 as PairingEngine>::G2Affine>>(&t_ser).unwrap();
+        assert_eq!(t2, t_deser);
+    }
+}
